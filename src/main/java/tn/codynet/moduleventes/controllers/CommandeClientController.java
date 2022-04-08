c5216db9b978ffa352a1604f2a84ff6247eed6a5
Amine <aminebouaziz20@gmail.com>
2022-04-08 15:35:16 +0100

    updated commande client

--- src/main/java/tn/codynet/moduleventes/controllers/CommandeClientController.java
+++ src/main/java/tn/codynet/moduleventes/controllers/CommandeClientController.java
@@ -1,6 +1,7 @@
 package tn.codynet.moduleventes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.codynet.moduleventes.entities.CommandeClient;
 import tn.codynet.moduleventes.entities.EtatCommande;
@@ -13,28 +14,48 @@ import java.util.Optional;
 public class CommandeClientController {
     @Autowired
     private CommandeClientServiceImpl commandeClientService;
+
     @PostMapping("/commandeClient/add")
-    CommandeClient addCommandeClient(@RequestBody CommandeClient commandeClient){
-        return commandeClientService.addCommandeClient(commandeClient);
-    };
+    ResponseEntity<CommandeClient> addCommandeClient(@RequestBody CommandeClient commandeClient) {
+        return ResponseEntity.ok(commandeClientService.addCommandeClient(commandeClient));
+    }
+
+    ;
+
     @GetMapping(path = "/commandeClients")
-    List<CommandeClient> findAll(){
-        return commandeClientService.findAll();
-    };
+    ResponseEntity<List<CommandeClient>> findAll() {
+        return ResponseEntity.ok(commandeClientService.findAll());
+    }
+
+    ;
+
     @GetMapping(path = "/commandeClient/{id}")
-    Optional<CommandeClient> findCommandeClientById(@PathVariable long id){
-        return commandeClientService.findCommandeClientById(id);
-    };
+    ResponseEntity<Optional<CommandeClient>> findCommandeClientById(@PathVariable long id) {
+        return ResponseEntity.ok(commandeClientService.findCommandeClientById(id));
+    }
+
+    ;
+
     @GetMapping(path = "/commandeClient/{ref}")
-    Optional<CommandeClient> findByReference(@PathVariable String ref){
-        return commandeClientService.findByReference(ref);
-    };
+    ResponseEntity<Optional<CommandeClient>> findByReference(@PathVariable String ref) {
+        return ResponseEntity.ok(commandeClientService.findByReference(ref));
+    }
+
+    ;
+
     @DeleteMapping(path = "commandeClient/{id}")
-    void deleteCommandeClient(long id){
-         commandeClientService.deleteCommandeClient(id);
-    };
-    @PatchMapping(path="/commandeClient/{id}/{etatCommande}")
-    CommandeClient updateEtat(@PathVariable("id") long id,@PathVariable("etatCommande") EtatCommande etatCommande){
-        return commandeClientService.updateEtat(id,etatCommande);
-    };
+     ResponseEntity<CommandeClient> deleteCommandeClient(long id) {
+        commandeClientService.deleteCommandeClient(id);
+
+        return (ResponseEntity) ResponseEntity.ok();
+    }
+
+    ;
+
+    @PatchMapping(path = "/commandeClient/{id}/{etatCommande}")
+    ResponseEntity<CommandeClient> updateEtat(@PathVariable("id") long id, @PathVariable("etatCommande") EtatCommande etatCommande) {
+        return ResponseEntity.ok(commandeClientService.updateEtat(id, etatCommande));
+    }
+
+    ;
 }
